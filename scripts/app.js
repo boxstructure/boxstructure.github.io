8b275616bfcfa213c5f75adfafa33f815bc0a6f4
boxstructure <mike.sun.18@gmail.com>
2015-01-20 04:31:51 -0600

    .

--- scripts/app.js
+++ scripts/app.js
@@ -322,6 +322,10 @@ function setHoverSJVC() {
 			},
 
 			mouseup: function(){
+
+			$(this).unbind("mouseover mouseleave");
+			$(setHoverNavback).bind("mouseover mouseleave");
+
 			setTimeout( function(){
 			$(mainTemplate).css('z-index', 0);}, 200);
 
@@ -331,8 +335,7 @@ function setHoverSJVC() {
 			TweenLite.to(bottomCover, .9, { delay:0, x:0, y:800, autoAlpha:1, ease:"easeOutExpo"});
 			TweenLite.to(bottomCover, .45, { delay:.2, autoAlpha:0});
 			
-			$(this).unbind("mouseover mouseleave");
-			$(setHoverNavback).bind("mouseover mouseleave");
+			
     	}
         });
 	}
