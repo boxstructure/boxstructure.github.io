c7a39f4b0f9552f9a6435c76f8b7d550cef01f3f
boxstructure <mike.sun.18@gmail.com>
2015-01-26 01:50:27 -0600

    .

--- scripts/app.js
+++ scripts/app.js
@@ -699,7 +699,10 @@ function setHoverKYRT() {
 			click: function(){
 			
 			var logoKyrt = $('#frame10').contents().find("#projectLogoTextkyrt");
-			TweenLite.to(logoKyrt, .5, { delay:.5, y:-100, autoAlpha:1, ease:"easeOutExpo"});
+			TweenLite.to(logoKyrt, .5, { delay:.3, x:50%, y:-100, autoAlpha:1, ease:"easeOutExpo"});
+
+			var desKyrt = $('#frame10').contents().find("#btKyrt");
+			TweenLite.to(desKyrt, .5, { delay:.3, x:50%, y:-100, autoAlpha:1, ease:"easeOutExpo"});
 
 			setTimeout( function(){
 
