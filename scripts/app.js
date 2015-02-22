ad71ad9b2af2b057857b10273b48fb2dc2ebe957
boxstructure <mike.sun.18@gmail.com>
2015-02-22 16:36:56 -0600

    .

--- scripts/app.js
+++ scripts/app.js
@@ -1151,6 +1151,8 @@ function setHoverNavback2() {
 				$(setHoverUVIA).bind("mouseover mouseleave");
 				$(setHoverMISC).bind("mouseover mouseleave");
 
+				document.getElementById('#frameC').contentDocument.location.reload(true);
+
 				setTimeout( function(){
 				$('#frame0').attr('src', $('#frame0').attr('src')); contentWindow.scrollTo(0,0);},
 				200)
